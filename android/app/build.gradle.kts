246bded4318b0a5867919015d7e3ac93c0f73307
Lo_Sguercio <davide.marchisio@edu.itspiemonte.it>
2025-05-19 17:18:35 +0200

    Revert "fix: flutterfire configuration"
    This reverts commit d424b92456511889188290c70da8959776a28b71.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,13 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.fondazione_casa_oze_esame_its"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -43,5 +42,3 @@ android {
 flutter {
     source = "../.."
 }
-
-
